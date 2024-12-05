288c88c7bc8eb5bff464b2b6b6b9376164a8b01d
gracemartin29 <grace.martin@student.norwichuni.ac.uk>
2024-12-05 10:58:21 +0000

    row

--- sketch.js
+++ sketch.js
@@ -1,7 +1,14 @@
 function setup() {
-  createCanvas(750, 700);
+  createCanvas(800, 700);
+  noLoop();
 }
 
 function draw() {
   background(220);
+  const CELLSIZE = 20;
+  noStroke();
+  for(let x = 0; x < width; x += CELLSIZE){
+    fill(random(255))
+    rect(x, 0, CELLSIZE)
+  }
 }
