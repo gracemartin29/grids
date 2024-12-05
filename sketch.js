0858a464581797cfc92b1d4f3ddfa6b5d8125641
gracemartin29 <grace.martin@student.norwichuni.ac.uk>
2024-12-05 11:31:39 +0000

    piet mondrian

--- sketch.js
+++ sketch.js
@@ -1,17 +1,48 @@
+const CELLSIZES = [50, 100, 150];
+const COLOURS = ["#C71585", "#483D8B", "#B0E0E6", "#000000", "#E6E6FA"]
+
 function setup() {
-  createCanvas(800, 700);
+  createCanvas(800, 600);
   noLoop();
 }
 
 function draw() {
   background(220);
-  const CELLSIZE = 20;
-  noStroke();
+  // noStroke();
+  // basic grid
+  /*const CELLSIZE = 20;
+  for (let y = 0; y < height; y += CELLSIZE) {
+    for (let x = 0; x < width; x += CELLSIZE) {
+      fill(random(255))
+      rect(x, y, CELLSIZE)
+    }
+  }*/
+
+  // piet mondrian inspired grid
+  /*let x = 0;
+  let y = 0;
+  let currentWidth = random(CELLSIZES);
+  let currentHeight = random(CELLSIZES);
+  strokeWeight(3);
+  while (y < height) {
+    let cellsize = random(CELLSIZES);
+    while (x < width) {
+      fill(random(COLOURS));
+      rect(x, y, currentHeight, currentHeight);
+      x += currentWidth;
+      currentWidth = random(CELLSIZES);
+    }
+    x = 0;
+    y += currentHeight;
+    currentHeight = random(CELLSIZES); 
+  }*/
+
+  // grid of shapes
+  const CELLSIZE = 50;
   for (let y = 0; y < height; y += CELLSIZE) {
     for (let x = 0; x < width; x += CELLSIZE) {
       fill(random(255))
       rect(x, y, CELLSIZE)
     }
   }
-
 }
